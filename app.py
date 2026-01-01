68d6a710e4cb89821cb28089da9292a1570ad7e6
Vishakha <vishakha_s@cs.iitr.ac.in>
2026-01-01 22:17:38 +0530

    Initial AutoJudge project with ML pipeline and Streamlit UI

--- app.py
+++ app.py
@@ -1,102 +1,59 @@
-import pandas as pd
+import streamlit as st
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
 from scipy.sparse import hstack
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression, LinearRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, mean_absolute_error, mean_squared_error
 
-df = pd.read_json("problems_dataset.jsonl" , lines = True)
-#print(df.isnull().sum())
-df["combined_text"] = ( df["title"] + " " + df["description"] + " " + df["input_description"]
-                        + " " + df["output_description"])
-df["combined_text"] = (df["combined_text"].str.lower().str.replace("\n", " ", regex=False).str.replace("\t", " ", regex=False))
-df["combined_text"] = df["combined_text"].str.replace(r"\s+", " ", regex=True)
+tfidf = pickle.load(open("models/tfidf_vectorizer.pkl", "rb"))
+clf_scaler = pickle.load(open("models/classification_scaler.pkl", "rb"))
+classifier = pickle.load(open("models/classification_model.pkl", "rb"))
+reg_scaler = pickle.load(open("models/regression_scaler.pkl", "rb"))
+regressor = pickle.load(open("models/regression_model.pkl", "rb"))
 
-##Feature Engineering
-df["text_length"] = df["combined_text"].str.len()
-#print(df["text_length"])
-math_symbols = r"[+\-*/=<>\^%]"
-df["symbol_count"] = df["combined_text"].str.count(math_symbols)
-#print(df["symbol_count"])
-keyword = ["graph", "dp", "dynamic programming", "tree", "recursion"]
-for kw in keyword:
-    pattern = r"\b" + kw + r"\b"
-    col_name = f"freq_{kw.replace(' ', '_')}"
-    df[col_name] = df["combined_text"].str.count(pattern)
+st.set_page_config(page_title="AutoJudge", layout="centered")
+st.title(" AutoJudge")
+st.subheader("Predict Programming Problem Difficulty")
+st.write("Paste the problem details below:")
+title = st.text_area("Problem Title")
+description = st.text_area("Problem Description")
+input_desc = st.text_area("Input Description")
+output_desc = st.text_area("Output Description")
 
-tfidf = TfidfVectorizer(max_features=3000, stop_words="english")
-X_tfidf = tfidf.fit_transform(df["combined_text"])
-numeric_features = df[["text_length", "symbol_count", "freq_graph", "freq_dp", "freq_dynamic_programming", "freq_tree",
-                        "freq_recursion"]].values
+#Feature Engineering
+def extract_features(text):
+    text = text.lower()
+    text_length = len(text)
+    symbol_count = sum(text.count(s) for s in "+-*/=<>&^%")
 
-###MODEL 1 - CLASSIFICATION
+    keywords = ["graph", "dp", "dynamic programming", "tree", "recursion"]
+    keyword_counts = [text.count(k) for k in keywords]
 
-y_class = df["problem_class"]
-X_train_tfidf, X_test_tfidf, y_train, y_test = train_test_split(
-    X_tfidf, y_class, test_size=0.2, random_state=42, stratify=y_class)
+    numeric_features = np.array(
+        [text_length, symbol_count] + keyword_counts
+    ).reshape(1, -1)
 
-num_train, num_test = train_test_split(
-    numeric_features, test_size=0.2, random_state=42, stratify=y_class)
+    return numeric_features
 
-#Scale numeric features 
-scaler = StandardScaler()
-num_train_scaled = scaler.fit_transform(num_train)
-num_test_scaled = scaler.transform(num_test)
+if st.button("Predict Difficulty"):
+    combined_text = f"{title} {description} {input_desc} {output_desc}"
 
-X_train = hstack([X_train_tfidf, num_train_scaled])
-X_test = hstack([X_test_tfidf, num_test_scaled])
+    if combined_text.strip() == "":
+        st.warning("Please enter problem details.")
+    else:
+       
+        text_tfidf = tfidf.transform([combined_text])
+        numeric = extract_features(combined_text)
 
-classifier = LogisticRegression(
-    max_iter=3000,
-    n_jobs=-1
-)
+        num_scaled_clf = clf_scaler.transform(numeric)
+        X_clf = hstack([text_tfidf, num_scaled_clf])
+        class_pred = classifier.predict(X_clf)[0]
 
-classifier.fit(X_train, y_train)
+        num_scaled_reg = reg_scaler.transform(numeric)
+        X_reg = hstack([text_tfidf, num_scaled_reg])
+        score_pred = regressor.predict(X_reg)[0]
+        st.success("Prediction Complete")
 
-y_pred = classifier.predict(X_test)
-print(y_pred[:10])
+        st.write("Predicted Difficulty Class")
+        st.write(f"**{class_pred.upper()}**")
 
-##model accuracy and confusion matrix
-accuracy = accuracy_score(y_test, y_pred)
-print("Classification Accuracy:", accuracy)
-
-cm = confusion_matrix(y_test, y_pred, labels=["easy", "medium", "hard"])
-#print(cm)
-
-###MODEL 2 - REGRESSION
-
-y_reg = df["problem_score"]
-print(y_reg.describe())
-
-X_train_tfidf, X_test_tfidf, y_train_reg, y_test_reg = train_test_split(
-    X_tfidf, y_reg, test_size=0.2, random_state=42)
-
-num_train, num_test = train_test_split(
-    numeric_features, test_size=0.2, random_state=42)
-
-scaler_reg = StandardScaler()
-num_train_scaled = scaler_reg.fit_transform(num_train)
-num_test_scaled = scaler_reg.transform(num_test)
-
-X_train_reg = hstack([X_train_tfidf, num_train_scaled])
-X_test_reg = hstack([X_test_tfidf, num_test_scaled])
-
-regressor = LinearRegression()
-regressor.fit(X_train_reg, y_train_reg)
-
-y_pred_reg = regressor.predict(X_test_reg)
-print(y_pred_reg[:10])
-
-mae = mean_absolute_error(y_test_reg, y_pred_reg)
-print("MAE:", mae)
-rmse = np.sqrt(mean_squared_error(y_test_reg, y_pred_reg))
-print("RMSE:", rmse)
-
-baseline_pred = np.full_like(y_test_reg, y_train_reg.mean())
-baseline_mae = mean_absolute_error(y_test_reg, baseline_pred)
-baseline_rmse = np.sqrt(mean_squared_error(y_test_reg, baseline_pred))
-
-print("Baseline MAE:", baseline_mae)
-print("Baseline RMSE:", baseline_rmse)
+        st.write("Predicted Difficulty Score")
+        st.write(f"**{round(score_pred, 2)}**")
